00bce67f8b4018ee14f3e30e25161f1f0f246247
Maksym <maxim.sivash@gmail.com>
2021-12-19 01:25:01 +0200

    change db structure (still need to test)

--- Vaccination_passport/Windows/MainWindow.xaml.cs
+++ Vaccination_passport/Windows/MainWindow.xaml.cs
@@ -24,18 +24,20 @@ namespace Vaccination_passport.Windows
 
         private void SearchButton_Click(object sender, RoutedEventArgs e)
         {
-            string name = SearchBox.Text;
+            string id = SearchBox.Text;
             DataAccess dataAccess = (DataAccess) App.Current.TryFindResource("SuperDB");
-            List<Person> people = dataAccess.GetPeople($"SELECT * FROM [Person] WHERE [full_name]='{name}';");
+            List<Person> people = dataAccess.GetPeople($"SELECT * FROM [Person] WHERE [ID]={id};");
 
 
             //MessageBox.Show(person.ToString());
             //перевірити, коли немає користувача
-            PassportWindow PassWindow = new PassportWindow(people [0]);
-            PassWindow.Resources ["currentPerson"] = people [0];
-            PassWindow.Show();
+            if (people.Count > 0)
+            {
+                PassportWindow PassWindow = new PassportWindow(people [0]);
+                PassWindow.Resources ["currentPerson"] = people [0];
+                PassWindow.Show();
 
-
+            }
         }
 
         private void TestButton_Click(object sender, RoutedEventArgs e)
